cffc710903d14b1680d7a6fbdca9ef7a3ed9c7f3
alibaransel <alibaransel25@gmail.com>
2024-03-19 00:24:43 +0300

    Implement neural network

--- car.js
+++ car.js
@@ -12,8 +12,13 @@ class Car {
         this.angle = 0;
         this.damaged = false;
 
+        this.uesBrain = controlType == "AI";
+
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
         }
         this.controls = new Controls(controlType);
     }
@@ -26,6 +31,17 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if (this.uesBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
